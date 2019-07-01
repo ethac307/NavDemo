943d3296a8103ccd48f1a4a5df6d594d7655bdc0
elaine <oreotrixy@gmail.com>
2019-07-01 10:36:49 -0500

    Adding files

---
+++ Navigation/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by student on 7/1/19.
+//  Copyright © 2019 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var screen: UILabel!
+    
+    @IBAction func cameraPressed(_ sender: Any) {
+        screen.text = "Camera button pressed"
+    }
+    @IBAction func hiPressed(_ sender: Any) {
+    }
+    @IBAction func item2Pressed(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
